5bdb19f9ef5c637744e7d40d2842dc758019f87d
aquiles099 <41967583+aquiles099@users.noreply.github.com>
2022-08-30 07:52:50 -0400

    Ajuste dashboard

--- src/app/views/home/dashboard/dashboard-gmao/dashboard-gmao.component.ts
+++ src/app/views/home/dashboard/dashboard-gmao/dashboard-gmao.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AuthService } from '@services/auth.service';
 import { VirtualScrollWithPaginatorComponent } from '../../../../shared/components/virtual-scroll-with-paginator/virtual-scroll-with-paginator.component';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-dashboard-gmao',
@@ -10,17 +12,24 @@ import { VirtualScrollWithPaginatorComponent } from '../../../../shared/componen
   ],
   encapsulation: ViewEncapsulation.None
 })
-export class DashboardGmaoComponent extends VirtualScrollWithPaginatorComponent implements OnInit {
+export class DashboardGmaoComponent  implements OnInit {
 
   public showSpinner:boolean = false;
 
   public records: any[] = [1,2,3];
 
-  constructor() {
-    super(null);
+  constructor(private _authService:AuthService) {
+    
+  }
+
+  get user():User
+  {
+    return this._authService.authUserService.user;
   }
 
   ngOnInit(): void {
+    console.log(this.user);
+    
   }
 
   onSelect(item: any): void {
